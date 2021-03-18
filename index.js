85e0917e3f19927135d4c7dad4c19c5e399c61e0
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-18 13:22:11 +0100

    successfuly connected to the database

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://memories:12345@cluster0.u2uu1.mongodb.net/memories?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Server running 5000');
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+mongoose.set('useFindAndModify', false);
