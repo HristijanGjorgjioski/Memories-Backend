7ba397f228c233eae344ca0b6c41bc92fef71f2c
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-19 06:45:47 +0100

    maked small change to index.js file

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-app.use('/posts', postRoutes);
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
@@ -16,6 +14,8 @@ app.use(cors());
 const CONNECTION_URL =
   'mongodb+srv://memories:12345@cluster0.u2uu1.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
+
+app.use('/posts', postRoutes);
 
 mongoose
   .connect(CONNECTION_URL, {
