d4f3246d112fda9d2bd80b217f3a61f2b4a708cb
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-20 09:18:15 +0100

    added like functinality

--- controllers/posts.js
+++ controllers/posts.js
@@ -43,12 +43,26 @@ export const updatePost = async (req, res) => {
 
 export const deletePost = async (req, res) => {
   const { id: _id } = req.params;
-  const post = req.body;
 
   if (!mongoose.Types.ObjectId.isValid(_id))
     return res.status(404).send('No post with that id');
 
-  const deletePost = await PostMessage.findByIdAndRemove(_id);
+  await PostMessage.findByIdAndRemove(_id);
 
   res.json({ message: 'Post deleted successfully' });
+};
+
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send('No post with that id');
+
+  const post = await PostMessage.findById(id);
+  const updatedPost = await PostMessage.findByIdAndUpdate(
+    id,
+    { likeCount: post.likeCount + 1 },
+    { new: true }
+  );
+  res.json(updatedPost);
 };
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ import {
   createPost,
   updatePost,
   deletePost,
+  likePost,
 } from '../controllers/posts.js';
 
 const router = express.Router();
@@ -16,5 +17,7 @@ router.post('/', createPost);
 router.patch('/:id', updatePost);
 
 router.delete('/:id', deletePost);
+
+router.patch('/:id/likePost', likePost);
 
 export default router;
