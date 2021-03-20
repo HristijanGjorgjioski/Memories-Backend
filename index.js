62eba366e39a4495a5db49664afcd5b206d68eb6
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-20 15:22:16 +0100

    added ENV and constnts, backend for now is done!

--- index.js
+++ index.js
@@ -2,23 +2,23 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  'mongodb+srv://memories:12345@cluster0.u2uu1.mongodb.net/memories?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 app.use('/posts', postRoutes);
 
 mongoose
-  .connect(CONNECTION_URL, {
+  .connect(process.env.CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
