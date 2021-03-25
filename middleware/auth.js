5be54e611bbd4185f4eba9e4d0ccadb11f4831c3
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-25 18:30:34 +0100

    App is almost done!

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,26 +1,28 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
+
+const secret = 'test';
 
 const auth = async (req, res, next) => {
-    try {
-        const token = req.header.authorization.split(" ")[1];
-        const isCustomAuth = token.length < 500;
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
 
-        let decodedData;
+    let decodedData;
 
-        if(token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'test');
+    if (token && isCustomAuth) {      
+      decodedData = jwt.verify(token, secret);
 
-            req.userId = decodedData?.id;
-        } else {
-            decodedData = jwt.decode(token);
+      req.userId = decodedData?.id;
+    } else {
+      decodedData = jwt.decode(token);
 
-            req.userId = decodedData?.sub;
-        }
+      req.userId = decodedData?.sub;
+    }    
 
-        next();
-    } catch (error) {
-        console.log(error);
-    }
-}
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 export default auth;
--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
-import { getPosts, createPost, updatePost, deletePost, likePost } from '../controllers/posts.js';
-import auth from '../middleware/auth.js'
+import { getPosts, getPost, createPost, updatePost, likePost, deletePost } from '../controllers/posts.js';
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
 router.get('/', getPosts);
 router.post('/', auth, createPost);
