a1ba2486e4d9a730ae8f0d6c03d837a1cb3f6219
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-03-26 07:36:25 +0100

    Final push

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
+
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
-import userRoutes from './routes/users.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
-dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }));
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
-
 app.use('/posts', postRoutes);
-app.use('/user', userRoutes);
+app.use("/user", userRouter);
 
-app.get('/', (req, res) => res.send('Hello to memories API'));
+const CONNECTION_URL = 'mongodb://root:4AacDf9Tva@memories-mongo-production/admin?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
 
-mongoose
-  .connect(process.env.CONNECTION_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => {
-      console.log('Server running 5000');
-    });
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
--- routes/posts.js
+++ routes/posts.js
@@ -6,7 +6,7 @@ const router = express.Router();
 import auth from "../middleware/auth.js";
 
 router.get('/', getPosts);
-router.post('/', auth, createPost);
+router.post('/',auth,  createPost);
 router.patch('/:id', auth, updatePost);
 router.delete('/:id', auth, deletePost);
 router.patch('/:id/likePost', auth, likePost);
